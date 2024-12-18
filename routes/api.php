23ad49eb5d0bf34a55a792c38177fd8dcd332177
yohanastg12 <112596583+yohanastg12@users.noreply.github.com>
2024-12-19 00:59:32 +0700

    membuat fitur form dan ulasan

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\GuideController;
+use App\Http\Controllers\ReviewController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+// Route untuk melihat semua ulasan
+Route::get('reviews', [ReviewController::class, 'getAllReviews'])->name('reviews.index');
+
+// Route untuk melihat ulasan berdasarkan ID report
+Route::get('reviews/report/{report}', [ReviewController::class, 'getReviewsByReport'])->name('reviews.report');
+
+// Route untuk membuat ulasan
+Route::post('reviews', [ReviewController::class, 'storeReviewAPI'])->name('reviews.store');
+
+// Route untuk menghapus ulasan berdasarkan ID review
+Route::delete('reviews/{review}', [ReviewController::class, 'deleteReviewAPI'])->name('reviews.delete');
+
+
+
 
 Route::prefix('guides')->name('api.guides.')->group(function () {
     Route::get('/', [GuideController::class, 'apiIndex'])->name('index');        // Tampilkan semua panduan
