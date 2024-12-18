4873ff23ada531e15ce27e2d961d3000959d202c
indahelisasihombing <indahelisasihombing>
2024-12-19 00:40:46 +0700

    fitur panduan dan api by indah

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\GuideController;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -13,44 +13,55 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
+
+Route::get('/lacak_dm', function () {
+    return view('lacak_dm');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/form', function () {
+    return view('form');
+})->name('form');
+
 Route::get('/beranda', function () {
     return view('beranda');
 })->name('beranda');
-
-// Route untuk duktek
-Route::get('/duktek', [ReportController::class, 'dashboard'])->name('dashboard');
-
-// Route untuk maintenance
-Route::get('/maintenance', [ReportController::class, 'dashboard'])->name('dashboard');
-
-Route::get('/dashboard', [ReportController::class, 'dashboard'])->name('dashboard');
 
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/guides/create', [GuideController::class, 'create'])->name('guides.create');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/panduan', [GuideController::class, 'userIndex'])->name('panduan');
+    Route::get('/user', [UserController::class, 'user'])->name('user');
+    Route::get('/guides/{guide}', [GuideController::class, 'show'])->name('guides.show');
+
+
+});
+
+
 
 // Rute untuk Logout
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/home', function () {
-    $user = Auth::user();
-    if ($user->role == 'duktek') {
-        return redirect()->route('duktek');
-    } elseif ($user->role == 'maintenance') {
-        return redirect()->route('maintenance');
-    } else {
-        return redirect()->route('user');
-    }
-})->name('home');
 
 // Rute untuk Admin (duktek dan maintenance)
 Route::middleware(['auth:admin'])->group(function () {
-    Route::get('/duktek', [AdminController::class, 'duktek'])->name('duktek');
-    Route::get('/maintenance', [AdminController::class, 'maintenance'])->name('maintenance');
-});
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/user', [UserController::class, 'user'])->name('user');
-});
+    Route::get('/guides', [GuideController::class, 'index'])->name('guides.index');
+    Route::delete('/guides/{guide}', [GuideController::class, 'destroy'])->name('guides.destroy');
+    Route::get('/guides/{guide}/edit', [GuideController::class, 'edit'])->name('guides.edit');
+    Route::put('/guides/{guide}', [GuideController::class, 'update'])->name('guides.update');
+    });
+    
+
+
+Route::post('/guides', [GuideController::class, 'store'])->name('guides.store');
+
+
